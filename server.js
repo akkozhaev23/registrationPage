d963607a452a93c9e4156d1cf45af8eef86d6a3f
akkozhaev23 <zhasik.mandarin@mail.ru>
2022-09-29 20:17:41 +0600

    second commit

--- server.js
+++ server.js
@@ -1,75 +1,69 @@
-const express = require('express')
-const formidable = require('express-formidable')
-const fs = require('fs')
+const express = require('express');
+const formidable = require('express-formidable');
+const fs = require('fs');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(formidable())
+app.use(express.json());
+app.use(formidable());
 
-const responseObj = {
+const responseObj = {};
 
-}
-
-
-const db = {
-
-}
-
+const db = {};
 
 app.post('/login', (req, res) => {
-    let base = fs.readFileSync('users.json', 'utf-8', (err, data) => {
-        if(err) {
-            console.log(err)
-        }
-        return data
-    })
-
-    let dannie = JSON.parse(base)
-
-    console.log(dannie)
-
-    for (let key in dannie) {
-        if (key === req.fields.userName && req.fields.userPassword === dannie[key]) {
-            responseObj.message = `Welcome, ${req.fields.userName}!`
-        } else {
-            responseObj.message = 'invalid login or password!'
-        }
-
+  let base = fs.readFileSync('users.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
     }
+    return data;
+  });
 
-    res.json(responseObj)
-})
+  let dannie = JSON.parse(base);
+
+  console.log(dannie);
+
+  if (
+    dannie.hasOwnProperty(req.fields.userName) &&
+    dannie[req.fields.userName] === req.fields.userPassword
+  ) {
+    responseObj.message = `Welcome, ${req.fields.userName}!`;
+  } else {
+    responseObj.message = 'invalid login or password!';
+  }
+
+  res.json(responseObj);
+});
 
 app.post('/signUp', (req, res) => {
-    let base = fs.readFileSync('users.json', 'utf-8')
+  let base = fs.readFileSync('users.json', 'utf-8');
 
-    let dannie = JSON.parse(base)
-    
+  let dannie = JSON.parse(base);
+
+  if (req.fields.userPassword === req.fields.rePassword) {
     if (dannie.hasOwnProperty(req.fields.userName)) {
-        responseObj.message = `User ${req.fields.userName} in use!`
+      responseObj.message = `User ${req.fields.userName} in use!`;
     } else {
-        let uName = req.fields.userName
-        let pass = req.fields.userPassword
-        let obj2 = {[uName]: pass}
-        Object.assign(dannie, obj2)
+      let uName = req.fields.userName;
+      let pass = req.fields.userPassword;
+      let obj2 = { [uName]: pass };
+      Object.assign(dannie, obj2);
 
-        let users = JSON.stringify(dannie)
+      let users = JSON.stringify(dannie);
 
-        fs.writeFileSync('users.json', users, err => console.log(err))
+      fs.writeFileSync('users.json', users, (err) => console.log(err));
 
-        responseObj.message = `New user ${req.fields.userName} is created`
+      responseObj.message = `New user ${req.fields.userName} is created`;
     }
+  } else {
+    responseObj.message = `Passwords do not match`;
+  }
 
+  res.json(responseObj);
+});
 
-    res.json(responseObj)
-})
-
-
-
-app.use(express.static('static'))
+// app.use(express.static('static'))
 
 app.listen(7000, () => {
-    console.log('Server listens on 7000 port')
-})
-
+  console.log('Server listens on 7000 port');
+});
